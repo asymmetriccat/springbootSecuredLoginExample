a69f3ceb4aa42eb8025bb057f6cbc1a39042183b
guilongli <guilongli@ymail.com>
2017-10-20 22:24:22 -0400

    first commit

---
+++ src/main/java/com/guilong/springSecurityLogin/SpringbootSecurityExamples1Application.java
@@ -0,0 +1,12 @@
+package com.guilong.springSecurityLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityExamples1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityExamples1Application.class, args);
+	}
+}
